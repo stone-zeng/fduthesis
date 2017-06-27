e57033b497d08e5deb63ebfc65399eae0f529c98
Stone-Zeng <pssysrq@163.com>
2017-06-27 13:52:26 +0800

    add: `fduthesis-user.def`

--- source/install.bat
+++ source/install.bat
@@ -1,4 +1,5 @@
 @echo off
 xetex fduthesis.ins
-move /Y "fduthesis.cls"  "..\test\fduthesis.cls"
-move /Y "fduthesis.def"  "..\test\fduthesis.def"
+move /Y "fduthesis.cls"       "..\test\fduthesis.cls"
+move /Y "fduthesis.def"       "..\test\fduthesis.def"
+move /Y "fduthesis-user.def"  "..\test\fduthesis-user.def"
