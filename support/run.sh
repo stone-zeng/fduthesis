3c212a8f29c471cd69f82a77b49ec0433637378e
Stone-Zeng <pssysrq@163.com>
2017-07-22 20:44:47 +0800

    view log

--- support/run.sh
+++ support/run.sh
@@ -9,20 +9,21 @@ cp fduthesis.def      ../test/fduthesis.def
 cp fduthesis-user.def ../test/fduthesis-user.def
 
 # Make logo
-cd ../logo
-pdflatex -interaction=nonstopmode fudan-emblem
-pdflatex -interaction=nonstopmode fudan-motto
-pdflatex -interaction=nonstopmode fudan-name
-xelatex  -interaction=nonstopmode fudan-emblem
-xelatex  -interaction=nonstopmode fudan-motto
-xelatex  -interaction=nonstopmode fudan-name
-lualatex -interaction=nonstopmode fudan-emblem
-lualatex -interaction=nonstopmode fudan-motto
-lualatex -interaction=nonstopmode fudan-name
+# cd ../logo
+# pdflatex -interaction=nonstopmode fudan-emblem
+# pdflatex -interaction=nonstopmode fudan-motto
+# pdflatex -interaction=nonstopmode fudan-name
+# xelatex  -interaction=nonstopmode fudan-emblem
+# xelatex  -interaction=nonstopmode fudan-motto
+# xelatex  -interaction=nonstopmode fudan-name
+# lualatex -interaction=nonstopmode fudan-emblem
+# lualatex -interaction=nonstopmode fudan-motto
+# lualatex -interaction=nonstopmode fudan-name
 
 # Make test theses
 cd ../test
 xelatex  -interaction=nonstopmode test
-lualatex -interaction=nonstopmode test
-xelatex  -interaction=nonstopmode test-en
-lualatex -interaction=nonstopmode test-en
+cat test.log
+# lualatex -interaction=nonstopmode test
+# xelatex  -interaction=nonstopmode test-en
+# lualatex -interaction=nonstopmode test-en
