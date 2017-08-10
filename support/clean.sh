87bbff50aef3288a047f3f251637ae9523ff4535
Stone-Zeng <pssysrq@163.com>
2017-08-10 12:13:15 +0800

    fix LuaTeX support; add option `normalem` for package `ulem`; remove `c_minus_one`; clean up code (trim spaces for arguments); add `clean.sh`, and restart test for LuaTeX;

---
+++ support/clean.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# Delete auxiliary files
+rm -f *.aux
+rm -f *.bak
+rm -f *.bbl
+rm -f *.blg
+rm -f *.fdb_latexmk
+rm -f *.fls
+rm -f *.glg
+rm -f *.glo
+rm -f *.gls
+rm -f *.hd
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.log
+rm -f *.out
+rm -f *.sav
+rm -f *.synctex.gz
+rm -f *.synctex\(busy\)
+rm -f *.thm
+rm -f *.toc
+
+# Delete target files
+rm -f test.pdf
+rm -f test-en.pdf
+rm -f *.dvi
+rm -f *.xdv
