2bb25537288fc441788f676d7edd13c6b909c9b2
Stone-Zeng <pssysrq@163.com>
2017-08-11 20:20:30 +0800

    add error messages; check old packaes (especially for the recent LaTeX3 breaking changes); unknown option will raise an error now; use `\mode_leave_vertical:` instead of `\mbox` (they are not the same thing in fact, but have the same effect); add `build.lua` (ready for l3build); doc: use `variable` envrionment for variables; add `int` labels

---
+++ build.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Build script for fduthesis
+
+module = "fduthesis"
+
+stdengine = "xetex"
+
+sourcefiles = {"source/*.dtx"}
+
+-- typesetdeps = {"source/latexmkrc."}
+typesetexe  = "xelatex"
+typesetfiles = {"fduthesis.dtx"}
+
+unpackfiles = {"fduthesis.dtx"}
+unpackexe   = "xetex"
+-- excludefiles = {"*/breqn -abbr-test.pdf",
+-- "*/eqbreaks.pdf"}
+-- unpackopts = "-interaction=batchmode"
+
+cmdchkfiles = {"*.dtx"}
+
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))
