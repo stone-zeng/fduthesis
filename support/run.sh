7177d05f09a8b263f3abfa8fbf05cf33bda2d346
Stone-Zeng <pssysrq@163.com>
2017-08-31 21:58:03 +0800

    feature: add logo files
    - `fdulogo-example.tex` is used to generate all logos, including
      PDF and PNG files. `fduthesis-cover.tex` is used to generate
      `fduthesis`'s cover. They are located in `fduthesis-logo.dtx`.
    - `logo` folder is no longer needed. Removed.
    - Add `color-palette`.
    - Add blank lines after `\section`, `\subsection` etc.
    - Update `README.md` and meta-comments.
    - Update travis ci test.

--- support/run.sh
+++ support/run.sh
@@ -3,16 +3,20 @@
 # Install `fduthesis`
 cd source
 xetex fduthesis.dtx
-cp fduthesis.cls      ../test/fduthesis.cls
-cp fduthesis-en.cls   ../test/fduthesis-en.cls
-cp fduthesis.def      ../test/fduthesis.def
-cp fduthesis-user.def ../test/fduthesis-user.def
+cp fduthesis.cls       ../test/fduthesis.cls
+cp fduthesis-en.cls    ../test/fduthesis-en.cls
+cp fduthesis.def       ../test/fduthesis.def
+cp fduthesis-user.def  ../test/fduthesis-user.def
+cp fdulogo.sty         ../logo/fdulogo.sty
+cp fdulogo-example.tex ../logo/fdulogo-example.tex
+cp fduthesis-cover.tex ../logo/fduthesis-cover.tex
 
 # Make logo
-cd ../logo
-pdflatex -interaction=nonstopmode fudan-emblem
-pdflatex -interaction=nonstopmode fudan-motto
-pdflatex -interaction=nonstopmode fudan-name
+# cd ../logo
+# pdflatex -interaction=nonstopmode -shell-escape fdulogo-example
+# pdflatex -interaction=nonstopmode               fduthesis-cover
+# rm fudan-*.pdf
+
 # xelatex  -interaction=nonstopmode fudan-emblem
 # xelatex  -interaction=nonstopmode fudan-motto
 # xelatex  -interaction=nonstopmode fudan-name
