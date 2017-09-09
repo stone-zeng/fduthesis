5dc2f45b65359bbb0c10f727fb4b13d37db45419
Stone-Zeng <pssysrq@163.com>
2017-09-09 20:55:14 +0800

    doc: add documentation in English [ci skip]
    - I use `%^^A!` as a label to indicate the English part, then use
      a simple lua script to get `fduthesis-en.tex`.
    - New setups need to be added.
    - I just do this as fun :smirk:. Only a little bit things are done.

---
+++ source/get-doc-en.lua
@@ -0,0 +1,23 @@
+local input_file_name  = "fduthesis.dtx"
+local output_file_name = "fduthesis-en.tex"
+
+local label_str = "%^^A!"
+local label_str_len = string.len(label_str)
+local space_str = " "
+local lf_str    = "\n"
+
+input_file  = io.open(input_file_name,  "r")
+output_file = io.open(output_file_name, "w")
+
+for line in input_file:lines() do
+    if string.sub(line, 1, label_str_len) == label_str then
+        new_line = space_str .. string.sub(line, label_str_len + 1) .. lf_str
+        if new_line == space_str .. lf_str then
+            new_line = lf_str
+        end
+        output_file:write(new_line)
+    end
+end
+
+input_file:close()
+output_file:close()
