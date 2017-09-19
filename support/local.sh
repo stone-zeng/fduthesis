09249c469a8201cbbae9e73eabaaa1721e5ea2bd
Stone-Zeng <pssysrq@163.com>
2017-09-19 08:10:46 +0800

    ci: remove locally installed `zhlipsum`

--- support/local.sh
+++ support/local.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env sh
 
-# Install `zhlipsum`
-git clone https://github.com/Stone-Zeng/zhlipsum.git
-
-cd zhlipsum/source
-xetex zhlipsum.ins
-
-cd ../..
-
-mkdir -p /home/travis/texmf/tex/latex/zhlipsum
-cp zhlipsum/source/zhlipsum.sty        \
-  /home/travis/texmf/tex/latex/zhlipsum
-cp zhlipsum/source/zhlipsum-zh-cn.def  \
-  /home/travis/texmf/tex/latex/zhlipsum
-
-mktexlsr
-
 # Install fonts
 # See https://tex.stackexchange.com/q/257231/136923 and
 # https://www.tug.org/texlive/doc/texlive-en/texlive-en.html#x1-340003.4.4
