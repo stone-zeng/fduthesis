15800f3983966b0c65043d30842f2994715010f4
Zeng Xiangdong <pssysrq@163.com>
2017-10-16 00:34:51 +0800

    ci: update `ptex-ng.sh`
    - Modify the path.

--- support/ptex-ng.sh
+++ support/ptex-ng.sh
@@ -12,7 +12,8 @@ tlmgr path add
 git clone https://github.com/clerkma/ptex-ng.git
 cd ptex-ng
 ./build-aptex.sh
-cp ptex-ng/aptex /tmp/texlive/bin/x86_64-linux
+cd /home/travis/.travis/build/Stone-Zeng/fduthesis
+cp ./ptex-ng/aptex /tmp/texlive/bin/x86_64-linux
 
 # For Windows
 # wget http://www.ring.gr.jp/archives/text/TeX/ptex-win32/current/ptex-ng-w32.tar.xz
