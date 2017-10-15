72876bddc97c0eea634fac5d92d486c5ec4020ab
Stone-Zeng <pssysrq@163.com>
2017-10-16 01:29:31 +0800

    ci: debug 4

--- support/ptex-ng.sh
+++ support/ptex-ng.sh
@@ -8,39 +8,22 @@ tlmgr pinning add tltexjp '*'
 tlmgr install ptex-ng
 tlmgr path add
 
-# Install newest version of pTeX-ng (from GitHub)
-if ! command -v aptex > /dev/null; then
-  echo "I have found aptex!"
-else
-  echo "I haven't found aptex!"
-fi
+rm /tmp/texlive/bin/x86_64-linux/ptex-ng
+rm /tmp/texlive/bin/x86_64-linux/platex-ng
 
-if ! command -v ptex-ng > /dev/null; then
-  echo "I have found ptex-ng!"
-else
-  echo "I haven't found ptex-ng!"
-fi
-
+# Build the newest version of pTeX-ng
 git clone https://github.com/clerkma/ptex-ng.git
 cd ptex-ng
 ./build-aptex.sh
-
-pwd
-ls -al
-cd src
-pwd
-ls -al
-
-cd /home/travis/build/Stone-Zeng/fduthesis
-cp ptex-ng/src/ptex-ng /tmp/texlive/bin/x86_64-linux
-
-cd /tmp/texlive/bin/x86_64-linux
-ls -al
-cd /home/travis/build/Stone-Zeng/fduthesis
+cd ..
 
 # For Windows
 # wget http://www.ring.gr.jp/archives/text/TeX/ptex-win32/current/ptex-ng-w32.tar.xz
 # tar -xJf ptex-ng-w32.tar.xz
 
 # Generate format files
+cp ptex-ng/src/ptex-ng /tmp/texlive/bin/x86_64-linux
 fmtutil-sys --byengine=ptex-ng
+
+# Chech information
+ptex-ng --version
