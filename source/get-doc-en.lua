b6ddcddb49ecdbf7578de3f2cf9120c52a8c1966
Stone-Zeng <pssysrq@163.com>
2017-10-19 21:29:19 +0800

    doc: update lua script [ci skip]
    - Add new tag `%^^A+` and `%^^A-`, as DocStrip guard `<*tag>` and `</tag>`.
    - Tidy up lua codes.
    - Avoid using `% ^^A`.

--- source/get-doc-en.lua
+++ source/get-doc-en.lua
@@ -1,8 +1,10 @@
 local input_file_name  = "fduthesis.dtx"
 local output_file_name = "fduthesis-en.tex"
 
-local label_str = "%^^A!"
-local label_str_len = string.len(label_str)
+local tag_inline_str = "%^^A!"
+local tag_begin_str  = "%^^A+"
+local tag_end_str    = "%^^A-"
+local tag_str_len    = string.len(tag_inline_str)
 
 local latex_ex_str = "\\begin{latexexample}["
 local shell_ex_str = "\\begin{shellexample}["
@@ -12,8 +14,18 @@ local latex_ex_str_len = string.len(latex_ex_str)
 local shell_ex_str_len = string.len(shell_ex_str)
 local syntax_str_len   = string.len(syntax_str)
 
-input_file  = io.open(input_file_name,  "r")
-output_file = io.open(output_file_name, "w")
+local function test_tag(str, tag)
+    if string.sub(str, 1, string.len(tag)) == tag then
+        return true
+    end
+    return false
+end
+
+local function remove_normal_space(str)
+    -- 普通文本开头会有 2 个空格
+    -- `3` 是第一个非空白字符的位置
+    return string.sub(str, 3)
+end
 
 local function add_lst_gobble(str)
     if string.sub(str, 1, latex_ex_str_len) == latex_ex_str then
@@ -31,20 +43,45 @@ local function add_lst_gobble(str)
     return str
 end
 
+local function process_verbatim_line(str)
+    -- 抄录环境
+    str = remove_normal_space(str)
+    str = add_lst_gobble(str)
+    return str
+end
+
+local function process_normal_line(str)
+    -- 普通文本行
+    str = string.sub(str, tag_str_len + 2)
+    str = add_lst_gobble(str)
+    return str
+end
+
+-----------------
+-- 开始处理文件
+-----------------
+
+input_file  = io.open(input_file_name,  "r")
+output_file = io.open(output_file_name, "w")
+
+-- 判断是否处于抄录环境
+tag_inside_flag = 0
+
 for line in input_file:lines() do
-    if string.sub(line, 1, label_str_len) == label_str then
-        new_line = " " .. string.sub(line, label_str_len + 1)
-        if new_line == " " then
-            -- 移除空行中的多余空格
-            new_line = ""
-        else
-            -- 移除其他行的行首空格
-            -- `3` 是第一个非空白字符的位置
-            new_line = string.sub(new_line, 3)
-            -- 添加 gobble 信息
-            new_line = add_lst_gobble(new_line)
+    -- 如果开头是 `%^^A+` 或 `%^^A-`，则增减 flag，以确定抄录开始、结
+    -- 束的位置
+    if test_tag(line, tag_begin_str) then
+        tag_inside_flag = tag_inside_flag + 1
+    elseif test_tag(line, tag_end_str) then
+        tag_inside_flag = tag_inside_flag - 1
+    else
+        -- 如果 flag = 1，则为抄录环境
+        if tag_inside_flag == 1 then
+            output_file:write(process_verbatim_line(line), "\n")
+        -- 如果开头是 `%^^A!`，则为普通文本行
+        elseif test_tag(line, tag_inline_str) then
+            output_file:write(process_normal_line(line), "\n")
         end
-        output_file:write(new_line, "\n")
     end
 end
 
