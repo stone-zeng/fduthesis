e099ea59e99eeb6fb6d090f9a25f858337006de7
Stone-Zeng <pssysrq@163.com>
2017-11-11 16:56:37 +0800

    ci: run with shell sciprt

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,34 +3,29 @@
 # Install `fduthesis`
 cd source
 xetex fduthesis.dtx
-cp fduthesis.cls       ../test/fduthesis.cls
-cp fduthesis-en.cls    ../test/fduthesis-en.cls
-cp fduthesis.def       ../test/fduthesis.def
-cp fduthesis-user.def  ../test/fduthesis-user.def
+cp fduthesis.cls       ../test/
+cp fduthesis-en.cls    ../test/
+cp fduthesis.def       ../test/
+cp fduthesis-user.def  ../test/
 
-mkdir ../logo
-cp fdulogo.sty         ../logo/fdulogo.sty
-cp fdulogo-example.tex ../logo/fdulogo-example.tex
-cp fduthesis-cover.tex ../logo/fduthesis-cover.tex
+mkdir                  ../logo/
+cp fdulogo.sty         ../logo/
+cp fdulogo-example.tex ../logo/
+cp fduthesis-cover.tex ../logo/
 
-# Make logo
-# cd ../logo
-# pdflatex -interaction=nonstopmode -shell-escape fdulogo-example
-# pdflatex -interaction=nonstopmode               fduthesis-cover
-# rm fudan-*.pdf
+# Make logos
+cd ../logo/
+pdflatex -interaction=nonstopmode -shell-escape fdulogo-example
+pdflatex -interaction=nonstopmode               fduthesis-cover
 
-# xelatex  -interaction=nonstopmode fudan-emblem
-# xelatex  -interaction=nonstopmode fudan-motto
-# xelatex  -interaction=nonstopmode fudan-name
-# lualatex -interaction=nonstopmode fudan-emblem
-# lualatex -interaction=nonstopmode fudan-motto
-# lualatex -interaction=nonstopmode fudan-name
+mkdir          pdf/
+mv fudan-*.pdf pdf/
+
+cd ../test/
 
 # Make test theses
-# The following commands will not give the errors correctly. So I
-# put them into `.travis.yml`.
-## cd ../test
-## xelatex  -interaction=nonstopmode test
-## lualatex -interaction=nonstopmode test
-## xelatex  -interaction=nonstopmode test-en
-## lualatex -interaction=nonstopmode test-en
+latexmk -xelatex  -f test
+latexmk -xelatex  -f test-en
+source ../scripts/clean.sh
+latexmk -lualatex -f test
+latexmk -lualatex -f test-en
