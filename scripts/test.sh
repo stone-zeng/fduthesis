09dfff186f62697f5b86525b2b5c22a870beda9c
Stone-Zeng <xdzeng96@gmail.com>
2018-02-27 19:22:17 +0800

    ci: use correct options for l3build

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ export TESTFILES="01-internal 02-basic 03-logo"
 
 texlua build.lua save --engine xetex  $TESTFILES
 texlua build.lua save --engine luatex $TESTFILES
-texlua build.lua save 03-logo
+texlua build.lua save "03-logo"
 
 texlua build.lua check
-texlua build.lua check -q -f -e pdftex 03-logo
+texlua build.lua check --quiet --force --engine pdftex "03-logo"
