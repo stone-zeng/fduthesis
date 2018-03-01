bee56861f0201f18634d99e5500afc917b6d7469
Stone-Zeng <xdzeng96@gmail.com>
2018-03-01 17:39:10 +0800

    test: add new tests
    - Add tests for cover, declaration page and English thesis.
    - Add `fduthesis-regression-test.tex`.
    - Use `--halt-on-error` option.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
-export TESTFILES="01-internal 02-basic 03-logo"
+export TESTFILES=\
+"
+01-internal
+02-basic
+03-basic-en
+04-cover
+05-declaration
+06-logo
+"
 
 texlua build.lua save --engine xetex  $TESTFILES
 texlua build.lua save --engine luatex $TESTFILES
-texlua build.lua save "03-logo"
+texlua build.lua save "06-logo"
 
-texlua build.lua check
-texlua build.lua check --quiet --force --engine pdftex "03-logo"
+# texlua build.lua check --halt-on-error
+# texlua build.lua check --halt-on-error --quiet --force --engine pdftex "06-logo"
